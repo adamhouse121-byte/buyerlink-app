32aa8b40b5c46bffbc667e08b725c71591e9cad4
adamhouse121-byte <adamhouse121@gmail.com>
2025-08-21 09:45:05 -0500

    Install Stripe SDK and add billing routes

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,4 +1,5 @@
 import Stripe from "stripe";
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20" as any,
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-06-20",
 });
