8ea2ae62cdb27342a93e8b72f199374a67c18f25
adamhouse121-byte <adamhouse121@gmail.com>
2025-08-22 14:28:53 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/a/:agentId/settings',
+        destination: 'https://app.buyerpref.link/moved-settings?agentId=:agentId',
+        permanent: false, // 307
+      },
+    ];
+  },
 };
 
 export default nextConfig;
